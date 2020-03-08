dd707b6c019f90bf961b58419f5c758331b1a207
GMadorell <gmadorell@gmail.com>
2020-03-08 23:16:14 +0100

    Add WordRepository to mediate between Room and your app

---
+++ android_room_with_a_view/app/src/main/java/com/skabed/roomwordsample/WordRepository.kt
@@ -0,0 +1,12 @@
+package com.skabed.roomwordsample
+
+import androidx.lifecycle.LiveData
+
+
+class WordRepository (private val wordDao: WordDao) {
+    val allWords: LiveData<List<Word>> = wordDao.getAlphabetizedWords()
+
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
+}
