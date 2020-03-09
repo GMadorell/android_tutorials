f810da9a665be9b828b38c5c0ba46b0515843fd6
GMadorell <gmadorell@gmail.com>
2020-03-09 23:01:20 +0100

    Add recycler view without linking it to any data whatsoever

--- android_room_with_a_view/app/src/main/java/com/skabed/roomwordsample/MainActivity.kt
+++ android_room_with_a_view/app/src/main/java/com/skabed/roomwordsample/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.skabed.roomwordsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Add the recyclerview
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val adapter = WordListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
